300cd858f63884017c0a520ed5c90cfe194d109e
AhmadKhawar <ahmadkhawar86@gmail.com>
2025-06-01 20:51:47 +0500

    Initial commit: added basic tracker with add/view

---
+++ tracker.py
@@ -0,0 +1,19 @@
+def add_expense():
+    amount = input("Enter amount: ")
+    category = input("Enter category: ")
+    with open("expenses.txt", "a") as f:
+        f.write(f"{category}: {amount}\n")
+
+def view_expenses():
+    with open("expenses.txt", "r") as f:
+        print(f.read())
+
+print("1. Add Expense")
+print("2. View Expenses")
+
+choice = input("Choose: ")
+
+if choice == "1":
+    add_expense()
+elif choice == "2":
+    view_expenses()
